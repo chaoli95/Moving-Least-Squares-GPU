0709f87128ebb88e3b3174f808ec4a7df20f5746
Chao Li <chaolinyc@gmail.com>
2023-05-02 00:05:06 -0400

    re format

--- main.cu
+++ main.cu
@@ -17,17 +17,17 @@ bool read_off(const std::string off_file,vector<T> &verticies,vector<T> &normals
         return false;
     }
     int nv,nf,ne;
-    input >> nv >> nf >> ne;
-    std::cout << nv << " " << nf << " " << ne << std::endl;
+    input>>nv>>nf>>ne;
+    std::cout<<nv<<" "<<nf<<" "<<ne<<std::endl;
     T tmp;
-    for(int v = 0; v<nv; ++v)
+    for(int v=0; v<nv; ++v)
     {
-        for(int i = 0; i<3; ++i)
+        for(int i=0; i<3; ++i)
         {
             input>>tmp;
             verticies.push_back(tmp);
         }
-        for(int i = 0; i<3; ++i)
+        for(int i=0; i<3; ++i)
         {
             input>>tmp;
             normals.push_back(tmp);
@@ -38,14 +38,14 @@ bool read_off(const std::string off_file,vector<T> &verticies,vector<T> &normals
 }
 
 template <typename T>
-void export_result(const std::string output_file_name, const vector<T> &X, const vector<T> &FX)
+void export_result(const std::string output_file_name,const vector<T> &X,const vector<T> &FX)
 {
-    std::cout << "writing to file " << output_file_name << std::endl;
+    std::cout<<"writing to file "<<output_file_name<<std::endl;
     std::ofstream output(output_file_name);
-    output << "OFF" << std::endl;
-    for (size_t i = 0; i < FX.size(); ++i)
+    output<<"OFF"<<std::endl;
+    for(size_t i=0; i<FX.size(); ++i)
     {
-        output << X[i * 3 + 0] << " " << X[i * 3 + 1] << " " << X[i * 3 + 2] << " " << FX[i] << std::endl;
+        output<<X[i*3+0]<<" "<<X[i*3+1]<<" "<<X[i*3+2]<<" "<<FX[i]<<std::endl;
     }
 }
 
@@ -56,18 +56,18 @@ int main(int argc,char *argv[])
         printf("usage: ./mls mesh.off [result.off]\n");
         exit(1);
     }
-    std::cout << argc << std::endl;
+    std::cout<<argc<<std::endl;
     vector<double> vertices,normals;
     if(!read_off(argv[1],vertices,normals))
     {
-        std::cout << "Error during read file " << argv[1] << std::endl;
+        std::cout<<"Error during read file "<<argv[1]<<std::endl;
         exit(1);
     }
-   
-    normals.resize(normals.size() / 3);
-    if(argc == 3)
+
+    normals.resize(normals.size()/3);
+    if(argc==3)
     {
-        std::cout << "Writing to file " << argv[2] << std::endl;
+        std::cout<<"Writing to file "<<argv[2]<<std::endl;
         export_result(argv[2],vertices,normals);
     }
     return 0;
